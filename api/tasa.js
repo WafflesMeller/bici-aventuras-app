dec9c5e152fceaba39355e76a977fc604dea7a7e
wafflesmeller <aleykisalfonzo@gmail.com>
2026-02-02 21:18:03 -0400

    arreglo para los montos bcv

--- api/tasa.js
+++ api/tasa.js
@@ -7,10 +7,14 @@ import https from "https";
 import * as cheerio from "cheerio";
 
 export default async function handler(req, res) {
-  try {
-    const agent = new https.Agent({
-      rejectUnauthorized: false,
-    });
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
     const response = await axios.get("https://www.bcv.org.ve/", {
       httpsAgent: agent,
